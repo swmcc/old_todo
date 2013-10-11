b52c00c441e5aaa24918511376b133bd7465d134
Stephen McCullough <stephen@sixdegreelabs.com>
2013-10-11 20:30:27 +0100

    Added ability to add a todo item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,9 @@
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addTodo = function() {
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    }
   });
