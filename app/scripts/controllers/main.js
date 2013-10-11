f1b6e9fdfbf654093597a04933781514b3dac788
Stephen McCullough <stephen@sixdegreelabs.com>
2013-10-11 20:25:41 +0100

    Added in todos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
   });
